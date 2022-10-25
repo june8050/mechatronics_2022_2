a44ee398557f44ba6e35e74d208d2adeb2031b69
june8050 <75426342+june8050@users.noreply.github.com>
2022-10-25 17:50:33 +0900

    Update main.c

--- midterm_project/main.c
+++ midterm_project/main.c
@@ -1,10 +1,8 @@
-
 #include <stdio.h>
 #include <wiringPi.h>
 #include <softPwm.h>
 
 //define requried things
-#define LOOPTIME 5 //sampling time
 #define ENCODERA 17 //Hall Sensor A
 #define ENCODERB 27  //Hall Sensor B
 #define ENC2REDGEAR 216
@@ -13,11 +11,12 @@
 #define MOTOR1 19 //GPIO19
 #define MOTOR2 26 //GPIO26
 
-#define PGAIN 100
-#define IGAIN 0
-#define DGAIN 0
-
+#define PGAIN 1000
+#define IGAIN 0.2
+#define DGAIN 100
 #define PULSEINPUT 18 //pulse
+
+#define SAMPLING_PERIOD 1 //millisecond
 
 // initialize
 int encA;
@@ -32,7 +31,6 @@ float errorPosition = 0;
 unsigned int checkTime;
 unsigned int checkTimeBefore;
 
-
 void funcEncoderA() //encoder position
 {
 	encA = digitalRead(ENCODERA);
@@ -42,12 +40,10 @@ void funcEncoderA() //encoder position
 		if (encB == LOW)
 		{
 			encoderPosition++;
-			//printf("CW");
 		}
 		else
 		{
 			encoderPosition--;
-			//printf("CCW");
 		}
 	}
 	else
@@ -55,17 +51,14 @@ void funcEncoderA() //encoder position
 		if (encB == LOW)
 		{
 			encoderPosition--;
-			//printf("CCW");
 		}
 		else
 		{
 			encoderPosition++;
-			//printf("CW");
 		}
 	}
 	redGearPosition = (float)encoderPosition / ENC2REDGEAR;
 	errorPosition = referencePosition - redGearPosition;
-	//printf("referencePos : %f / redGearPos : %f / errorPos : %f\n", referencePosition, redGearPosition, errorPosition);
 }
 void funcEncoderB() //encoder position 
 {
@@ -76,12 +69,10 @@ void funcEncoderB() //encoder position
 		if (encA == LOW)
 		{
 			encoderPosition--;
-			//printf("CCW");
 		}
 		else
 		{
 			encoderPosition++;
-			//printf("CW");
 		}
 	}
 	else
@@ -89,20 +80,17 @@ void funcEncoderB() //encoder position
 		if (encA == LOW)
 		{
 			encoderPosition++;
-			//printf("CW");
 		}
 		else
 		{
 			encoderPosition--;
-			//printf("CCW");
 		}
 	}
 	redGearPosition = (float)encoderPosition / ENC2REDGEAR;
 	errorPosition = referencePosition - redGearPosition;
-	//printf("referencePos : %f / redGearPos : %f / errorPos : %f\n", referencePosition, redGearPosition, errorPosition);
 }
 
-
+//To make the control process more precise, 
 void MoterReady()
 {
 
@@ -122,9 +110,6 @@ void MoterReady()
 
 
 int main() {
-	FILE  *fp;
-	fp = fopen( "test.txt", "w");
-	
 	wiringPiSetupGpio();
 	pinMode(ENCODERA, INPUT);		// Set ENCODERA as input
 	pinMode(ENCODERB, INPUT);		// Set ENCODERB as input
@@ -138,7 +123,6 @@ int main() {
 
 	MoterReady();
 
-
 	printf("Write total number of trials : ");
 	int total;
 	scanf("%d", &total);
@@ -147,75 +131,99 @@ int main() {
 		scanf("%d", &total);
 	}
 	printf("Total number of trials : %d\n", total);
+
 	int target[total];
+
 	for (int i = 0; i < total; ++i) {
 		printf("Write %d th target location : ", i + 1);
 		scanf("%d", &target[i]);
 	}
+
 	int count = 0;
 	float currentPosition = 0;
 	int pulse = 0;
-	while (!pulse){
+
+	while (!pulse) {
 		pulse = digitalRead(PULSEINPUT);
-		}
-	int helper = 0;
+	}
+
 	while (count < total)
 	{
-		printf("%d trail ----------------------------------------\n",count);
+		unsigned int checkTimeBefore = millis();
+		unsigned int checkTimeBefore4last = 0;
+
+		printf("%d trial ----------------------------------------\n", count + 1);
 		referencePosition = target[count] - currentPosition;
 		redGearPosition = 0; // regress redGearPosition to referencePosition
-		errorPosition = referencePosition - redGearPosition;
-		
+		encoderPosition=0;
+		errorPosition = referencePosition;
+
 		float errorSum = 0;
 		float previousError = errorPosition;
 
-		unsigned int checkTimeBefore = millis();
 		pulse = 0;
+
+		int pulseOn = 1;
+
+		//마지막 시행에서 5초 뒤 프로그램 종료하기 위함
+		if (count == total - 1) {
+			checkTimeBefore4last = millis();
+		}
+
+		//각 시행에 대해 새로운 펄스 들어올때 까지 제어 실행
 		while (1)
 		{
 			pulse = digitalRead(PULSEINPUT);
-			if (pulse && errorPosition < 0.1 && errorPosition > -0.1){
+
+			if ( !pulseOn && pulse ) {
 				break;
 			}
-			
-			/*
+
+			if (pulse) pulseOn = 1;
+			else if (!pulse) pulseOn = 0;
+
 			unsigned int checkTime = millis();
-			float deltaTime = (float) (checkTime - checkTimeBefore)/1000;
 
-			errorSum += deltaTime * errorPosition;
+			if (checkTime - checkTimeBefore >= SAMPLING_PERIOD) {
 
-			float derivativeError = (errorPosition - previousError) / deltaTime;
 
-			float controlledPWM = PGAIN * errorPosition + IGAIN * errorSum + DGAIN * derivativeError;
-			*/
-			
-			if (helper % 10000 == 0) {
-			fprintf( fp, "referencePos : %f / redGearPos : %f / errorPos : %f\n", referencePosition, redGearPosition, errorPosition );
+				float deltaTime = (float)SAMPLING_PERIOD / 1000;
 
-			printf("referencePos : %f / redGearPos : %f / errorPos : %f\n", referencePosition, redGearPosition, errorPosition);
+				errorSum += deltaTime * errorPosition;
+
+				float differentialError = (errorPosition - previousError) / deltaTime;
+
+				float controlledPWM = PGAIN * errorPosition + IGAIN * errorSum + DGAIN * differentialError;
+
+				if (errorPosition > 0)
+				{
+					softPwmWrite(MOTOR1, controlledPWM);
+					softPwmWrite(MOTOR2, 0);
+				}
+				else
+				{
+					softPwmWrite(MOTOR2, -controlledPWM);
+					softPwmWrite(MOTOR1, 0);
+				}
+
+				if (count == total - 1) {
+					unsigned int checkTime4last = millis();
+					if (checkTime4last - checkTimeBefore4last >= 5000) {
+						break;
+					}
+				}
+
+				previousError = errorPosition;
+				checkTimeBefore = checkTime;
 			}
-			helper++;
-			
-			if (errorPosition > 0)
-			{
-				softPwmWrite(MOTOR1, errorPosition * PGAIN);
-				softPwmWrite(MOTOR2, 0);
-			}
-			else
-			{
-				softPwmWrite(MOTOR2, -errorPosition * PGAIN);
-				softPwmWrite(MOTOR1, 0);
-			}
-
-			previousError = errorPosition;
-			checkTimeBefore = checkTime;
-
 		}
 		currentPosition = target[count];
 		count++;
 	}
-	
-			fclose(fp);
+
+	fclose(fp);
 
 	return 0;
 }
+
+
